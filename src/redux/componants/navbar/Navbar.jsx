828b517b8c2b5512189f98b16c4f055ecb702d01
mookbin <beeen1@naver.com>
2023-05-10 00:09:10 +0900

    Feat : 지역별 글 조회 및 지역 내 카테고리 조회 기능 완료

--- src/redux/componants/navbar/Navbar.jsx
+++ src/redux/componants/navbar/Navbar.jsx
@@ -1,29 +1,55 @@
 import { AiTwotoneHome, AiFillCaretLeft } from "react-icons/ai";
-
+import { useNavigate } from "react-router";
+import { Navhead, Logintext, Navtext } from "./styles";
 function Navbar(props) {
-    const navigate = useNavigate();
-    return (
-        <>
-            <Navhead>
-                <div style={{ display: 'flex', alignItems: 'center', justifyContent:'space-between', width:'4%' }}>
-                    <AiTwotoneHome
-                        style={{
-                            fontSize: '30px'
-                        }}
-                        onClick={() => { navigate('/') }} />
-                    <AiFillCaretLeft
-                        onClick={() => { navigate(-1) }}
-                        style={{ fontSize: '30px' }} />
-                </div>
-                <Navtext>{props.children}</Navtext>
-                {
-                    props.isActive === true ?
-                        <Logintext onClick={() => { navigate('/login') }}>로그인</Logintext> :
-                        <Logintext onClick={() => { navigate('/login') }}>로그아웃</Logintext>
-                }
-            </Navhead>
-        </>
-    )
+  const navigate = useNavigate();
+  return (
+    <>
+      <Navhead>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-between",
+            width: "4%",
+          }}
+        >
+          <AiTwotoneHome
+            style={{
+              fontSize: "30px",
+            }}
+            onClick={() => {
+              navigate("/");
+            }}
+          />
+          <AiFillCaretLeft
+            onClick={() => {
+              navigate(-1);
+            }}
+            style={{ fontSize: "30px" }}
+          />
+        </div>
+        <Navtext>{props.children}</Navtext>
+        {props.isActive === true ? (
+          <Logintext
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            로그인
+          </Logintext>
+        ) : (
+          <Logintext
+            onClick={() => {
+              navigate("/login");
+            }}
+          >
+            로그아웃
+          </Logintext>
+        )}
+      </Navhead>
+    </>
+  );
 }
 
 export default Navbar;
