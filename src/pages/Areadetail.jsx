417be6a3a44c1b473fb49d2afde5c6a380aba75f
mookbin <beeen1@naver.com>
2023-05-10 11:17:57 +0900

    Comment : 주석 및 console.log 삭제

--- src/pages/Areadetail.jsx
+++ src/pages/Areadetail.jsx
@@ -16,7 +16,6 @@ import { useQuery } from "react-query";
 import { useLocation } from "react-router-dom";
 function Areadetail() {
   const location = useLocation();
-  ////  route state를 받아옴. 전달된 region 정보가 있는지 확인
   const region = location.state?.region;
   const category = location.state?.category;
   const navigate = useNavigate();
@@ -37,7 +36,6 @@ function Areadetail() {
       (posts) =>
         posts.category === category && posts.region === localStorageRegion
     );
-    //   console.log(filteredData);
   }
   if (category === "") {
     filteredData = filteredData.filter(
@@ -47,7 +45,7 @@ function Areadetail() {
 
   return (
     <>
-      <Navbar isActive={false}>동네 게시판</Navbar>
+      <Navbar isActive={true}>동네 게시판</Navbar>
 
       <div style={{ display: "flex", justifyContent: "space-between" }}>
         <Menu />
@@ -69,7 +67,6 @@ function Areadetail() {
         style={{
           marginTop: "50px",
           height: "600px",
-          // border: "1px solid black",
         }}
         slidesPerView={1}
         centeredSlides={false}
@@ -94,9 +91,6 @@ function Areadetail() {
         modules={[Keyboard, Navigation, Pagination]}
         className="mySwiper"
       >
-        {/* slice() 함수를 사용해서 배열을 복사하여 새로운 배열을 반환
-        reverse() 함수로 새 배열의 순서를 뒤집기. 그 후 map() 함수를 호출해서 최신순으로 정렬함 */}
-        {/* 받아온 region 정보를 이용해서 data를 필터링 */}
         {filteredData
           .slice()
           .reverse()
@@ -126,7 +120,7 @@ function Areadetail() {
                   {/* // src={`http://서버주소/uploads/${data.id}.jpg`} */}
                   {/* // alt="example" // /> */}
                 </div>
-                {/* <p style={{ marginLeft: "10px" }}>좋아요 수{posts.goodCount}</p> */}
+
                 <p style={{ marginLeft: "10px" }}>지역 {posts.region}</p>
                 <p style={{ marginLeft: "10px" }}>작성시간{posts.createdAt}</p>
                 <p style={{ marginLeft: "10px" }}>작성자 {posts.nickname}</p>
